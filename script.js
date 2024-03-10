2b882a9419f8703529825c510fb372708f12a291
MatheGenie27 <78275290+MatheGenie27@users.noreply.github.com>
2024-03-10 20:29:17 +0100

    Grund Funktionalität hergestellt

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //Variablen
 
 // Warenkorb
-let basketPosition = 0;
+
 let basketMenuID = [];
 let basketMenuAmount = [];
 let summedPrice=[];
@@ -60,8 +60,7 @@ function renderBasket(){
         
 
         
-        //table.innerHTML += basketTableHTML();
-
+        
         for(let index = 0; index < basketMenuID.length; index++){
             
             table.innerHTML += basketContentHTML(index);
@@ -118,14 +117,14 @@ function categoryyHTML(index){
 
 
 function basketContentHTML(index){
-    console.log(`Füge Tabelleninhalt hinzu für Index ${index}`)
+    
     return `
         <tr>
-            <td><div class="amountButtonBasket" id="" onclick="deleteFromBasket(${basketMenuID[index]})">-</div></td>
-            <td>${basketMenuAmount[index]} </td>
-            <td><div class="amountButtonBasket" id="" onclick="addToBasket(${basketMenuID[index]})">+</div></td>
+            <td width="30"><div class="amountButtonBasket" id="" onclick="deleteFromBasket(${basketMenuID[index]})">-</div></td>
+            <td align=center width="30">${basketMenuAmount[index]} </td>
+            <td width="30"><div class="amountButtonBasket" id="" onclick="addToBasket(${basketMenuID[index]})">+</div></td>
             <td>${getMenuName(basketMenuID[index])} </td>
-            <td>${summedPrice[index]} </td>
+            <td align=right width="100">${summedPrice[index].toFixed(2)}€ </td>
         </tr>
     
 
@@ -135,16 +134,23 @@ function basketContentHTML(index){
 function basketCalcHTML(){
      return `
      <tr>
+        <td colspan=5><hr></td>
+     </tr>
+     <tr>
         <td colspan="4">Zwischensumme </td>
-        <td> ${subtotal} </td>
+        <td align=right> ${subtotal.toFixed(2)}€ </td>
      </tr>
      <tr>
         <td colspan="4">Lieferung </td>
-        <td> ${deliveryCost}</td>
+        <td align=right> ${deliveryCost.toFixed(2)}€</td>
+     </tr>
+     <tr>
+        <td colspan=4> </td>
+        <td ><hr></td>
      </tr>
      <tr>  
         <td colspan="4">Total</td>
-        <td> ${total}</td> 
+        <td align=right> ${total.toFixed(2)}€</td> 
      </tr>
      `;
 }
@@ -153,7 +159,7 @@ function basketFootHTML(){
     return `
    
 
-    <div id="basketFoot">Diese Summe beinhaltet 7% Mehrwertsteuer in Höhe von ${tax}.</div>
+    <div id="basketFoot">Diese Summe beinhaltet 7% Mehrwertsteuer in Höhe von ${tax.toFixed(2)} €.</div>
     `;
 }
 
@@ -168,7 +174,7 @@ function menuHTML(index){
                 ${menus[index].menuName}
             </div>
             <div class= "menuContainerAddButton" id="addButton${menus[index].menuID}" onclick="addToBasket(${menus[index].menuID})">
-                addButton
+                ${menuAddSymbol(menus[index].menuID)}
             </div>
         </div>
         <div class="menuContainerDescriptionRow">
@@ -204,31 +210,68 @@ function getMenuName(id){
 
 //Datenmanipulation
 
+function menuAddSymbol(ID){
+    if (getIndexOfBasket(ID)>-1){
+        return basketMenuAmount[getIndexOfBasket(ID)];
+    } else {return '+'}
+}
+
+
 function addToBasket(thisID){
       
     for (let i = 0; i < menus.length; i++){
         if (menus[i].menuID == thisID){
-            basketPosition++;
+            
+            if(getIndexOfBasket(thisID)>-1){
+                let index = getIndexOfBasket(thisID);
+                basketMenuAmount[index]=basketMenuAmount[index]+1;
+                
+            } else {            
             basketMenuID.push(thisID);
             basketMenuAmount.push(1);
+            }
         }
     
     }
-    console.log(`Es wurden hinzugefügt ID ${basketMenuID}, Menge +1`);
     
     
     calcBasket();
     renderBasket();
+    renderContent();
     
 
 }
 
+
+
+function getIndexOfBasket(ID){
+    
+    for (let index = 0; index < basketMenuID.length; index++){
+        
+        if(basketMenuID[index]==ID){
+            console.log()
+            return index;
+        } 
+    }  return -1;
+    
+    
+}
+
 function deleteFromBasket(element){
 
+    let index = getIndexOfBasket(element);
+    
+    if (basketMenuAmount[index]>1){
+        basketMenuAmount[index]=basketMenuAmount[index]-1;
+    } else { 
+        basketMenuAmount.splice(index, 1);
+        basketMenuID.splice(index,1);
+    }
 
-    console.log(`Es wurden entfernt ID ${basketMenuID}, Menge -1}`);
+    
     calcBasket();
     renderBasket();
+    renderContent();
 }
 
 function calcSummedPrice(){
@@ -258,11 +301,10 @@ function calcBasket(){
 function calcSubTotal(){
    subtotal = 0;
     for (let i =0; i<summedPrice.length; i++){
-        console.log("Rechenschritt Zwischensumme");
+        
         subtotal = Math.round((subtotal + summedPrice[i])*100)/100;
     }  
-    console.log(summedPrice)
-    console.log(`Zwischensumme ${subtotal}`);
+    
 
 }
 
@@ -285,7 +327,7 @@ let menus = [
 
     {
         type: 'category',
-        categoryName: 'categoryName',
+        categoryName: 'Vorspeisen',
         categoryImage: 'urlCategoryImage'
 
     },
@@ -300,9 +342,6 @@ let menus = [
     menuDescription: 'menuDescription1',
     menuImage: 'ImageUrl1',
     menuPrice: '14.99'
-
-
-
     },
 
     {type:'menu',    
@@ -311,7 +350,103 @@ let menus = [
     menuName: 'menuName2',
     menuDescription: 'menuDescription1',
     menuImage: 'ImageUrl2',
-    menuPrice: '17.99'}
+    menuPrice: '17.99'
+    },
+
+    {type:'menu',    
+    menuID: 3,
+    menuCategory: 'category1',    
+    menuName: 'menuName3',
+    menuDescription: 'menuDescription1',
+    menuImage: 'ImageUrl2',
+    menuPrice: '17.99'
+    },
+
+
+
+    {
+        type: 'category',
+        categoryName: 'Hauptgerichte',
+        categoryImage: 'urlCategoryImage'
+
+    },
+
+    {  
+        type:'menu',    
+        menuID: 4,
+        menuCategory: 'category2',    
+        menuName: 'menuName4',
+        menuDescription: 'menuDescription1',
+        menuImage: 'ImageUrl1',
+        menuPrice: '14.99'
+        },
+    
+        {type:'menu',    
+        menuID: 5,
+        menuCategory: 'category2',    
+        menuName: 'menuName5',
+        menuDescription: 'menuDescription1',
+        menuImage: 'ImageUrl2',
+        menuPrice: '17.99'
+        },
+    
+        {type:'menu',    
+        menuID: 6,
+        menuCategory: 'category2',    
+        menuName: 'menuName6',
+        menuDescription: 'menuDescription1',
+        menuImage: 'ImageUrl2',
+        menuPrice: '17.99'
+        },
+
+        {type:'menu',    
+        menuID: 7,
+        menuCategory: 'category2',    
+        menuName: 'menuName7',
+        menuDescription: 'menuDescription1',
+        menuImage: 'ImageUrl2',
+        menuPrice: '17.99'
+        },
+
+
+        {
+            type: 'category',
+            categoryName: 'Nachtisch',
+            categoryImage: 'urlCategoryImage'
+    
+        },
+    
+    
+    
+        {  
+        type:'menu',    
+        menuID: 8,
+        menuCategory: 'category3',    
+        menuName: 'menuName8',
+        menuDescription: 'menuDescription1',
+        menuImage: 'ImageUrl1',
+        menuPrice: '7.99'
+        },
+    
+        {type:'menu',    
+        menuID: 9,
+        menuCategory: 'category3',    
+        menuName: 'menuName9',
+        menuDescription: 'menuDescription1',
+        menuImage: 'ImageUrl2',
+        menuPrice: '5.99'
+        },
+    
+        {type:'menu',    
+        menuID: 10,
+        menuCategory: 'category3',    
+        menuName: 'menuName10',
+        menuDescription: 'menuDescription1',
+        menuImage: 'ImageUrl2',
+        menuPrice: '12.99'
+        }
+
+
 ];
 
 
